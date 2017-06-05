d884114814aacfc901db18ad68bff227191a365a
Taylor Burgess <tayloraburgess@gmail.com>
2017-06-04 22:26:13 -0400

    Basic view structure

--- main.elm
+++ main.elm
@@ -1,24 +1,47 @@
-import Html exposing (beginnerProgram, div, text)
+import Html exposing (Html, beginnerProgram, div, text)
 import Html.Attributes exposing (style)
-import Css exposing (asPairs, height, width, backgroundColor, rgb, px)
+import List exposing (map)
+import Css exposing (asPairs, display, height, width, backgroundColor, rgb, px, inlineBlock)
 
 
 main =
-  beginnerProgram { model = 0, view = view, update = update }
+  beginnerProgram { model = model, view = view, update = update }
 
-type Msg = Increment | Decrement
+
+type Cell =
+  Alive | Dead
+
+type alias Row =
+  List Cell
+
+type alias Board =
+  List Row
+
+type Msg = Nothing 
+
+model =
+  [ [ Alive, Dead ], [ Dead, Alive ] ]
 
 
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    Nothing ->
+      model 
 
-    Decrement ->
-      model - 1
 
 styles =
   asPairs >> style
 
+renderCell : Cell -> Html msg 
+renderCell cell =
+  if cell == Alive then
+    div [ styles [ display inlineBlock, width (px 20), height (px 20), backgroundColor (rgb 255 0 0) ] ]  [ ]
+  else
+    div [ styles [ display inlineBlock, width (px 20), height (px 20), backgroundColor (rgb 255 255 255) ] ]  [ ]
+
+renderRow : Row -> Html msg
+renderRow row =
+  div [ styles [ height (px 20) ] ] (map renderCell row)
+
 view model =
-  div [ styles [ backgroundColor (rgb 255 0 0), width (px 50), height (px 50)] ] [ text "testing" ]
+  div [ ] (map renderRow model)
